253f740932e37989988604fa45760383a69d1184
Teddy Heinen <skyshayde@gmail.com>
2019-08-27 22:21:17 -0500

    work at getting something working with the normal gatt library

--- app/src/main/java/fueltheburn/fueltheburn/LeScanCallback.kt
+++ app/src/main/java/fueltheburn/fueltheburn/LeScanCallback.kt
@@ -1,16 +1,23 @@
 package fueltheburn.fueltheburn
 
+import android.bluetooth.*
+import android.bluetooth.BluetoothAdapter.STATE_CONNECTED
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.os.Bundle
-import com.beepiz.bluetooth.gattcoroutines.GattConnection
-import fueltheburn.fueltheburn.extensions.authenticate
+import android.util.Log
 import fueltheburn.fueltheburn.extensions.getAdvertisementData
-
+import fueltheburn.fueltheburn.extensions.toHexString
+import java.util.*
 
 
 object LeScanCallback : ScanCallback() {
 
+    private val TAG = "fueltheburn"
+    private val COPPERHEAD_COMMAND_UUID = UUID.fromString("c7d25540-31dd-11e2-81c1-0800200c9a66")
+    private val COPPERHEAD_RESPONSE_UUID = UUID.fromString("d36f33f0-31dd-11e2-81c1-0800200c9a66")
+    private val COPPERHEAD_SERVICE_UUID = UUID.fromString("83cdc410-31dd-11e2-81c1-0800200c9a66")
+    private val CHARACTERISTIC_UPDATE_NOTIFICATION_DESCRIPTOR_UUID = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
 
     private val NIKE_COMPANY_IDENTIFIER = byteArrayOf(0, 120) // 0x0078
     private val connectedDevices: MutableList<String> = mutableListOf()
@@ -21,8 +28,44 @@ object LeScanCallback : ScanCallback() {
         val companyCode: ByteArray = advertisementData.getByteArray("COMPANYCODE") ?: byteArrayOf(0, 0)
         if (companyCode contentEquals NIKE_COMPANY_IDENTIFIER && !connectedDevices.contains(result.device.address)) {
             connectedDevices.add(result.device.address)
-            val deviceConnection: GattConnection = GattConnection(result.device)
-            deviceConnection.authenticate()
+            var bluetoothGatt: BluetoothGatt? = null
+            bluetoothGatt = result.device.connectGatt(MainActivity.context, false, object : BluetoothGattCallback() {
+                override fun onConnectionStateChange(
+                        gatt: BluetoothGatt,
+                        status: Int,
+                        newState: Int
+                ) {
+                    when (newState) {
+                        BluetoothProfile.STATE_CONNECTED -> {
+                            Log.i(TAG, "Connected to GATT server.")
+                            Log.i(TAG, "Attempting to start service discovery: " +
+                                    gatt?.discoverServices())
+
+                        }
+                        BluetoothProfile.STATE_DISCONNECTED -> {
+                            Log.i(TAG, "Disconnected from GATT server.")
+                        }
+                    }
+                }
+
+                override fun onServicesDiscovered(gatt: BluetoothGatt?, status: Int) {
+                    val service = gatt?.getService(COPPERHEAD_SERVICE_UUID)
+                    val cmd = service?.getCharacteristic(COPPERHEAD_COMMAND_UUID)
+                    val resp = service?.getCharacteristic(COPPERHEAD_RESPONSE_UUID)
+                    gatt?.setCharacteristicNotification(resp, true)
+                    val descriptor = resp?.getDescriptor(CHARACTERISTIC_UPDATE_NOTIFICATION_DESCRIPTOR_UUID);
+                    descriptor?.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE)
+                    gatt?.writeDescriptor(descriptor);
+                    cmd?.value = byteArrayOf(0x90.toByte(), 0x01.toByte(), 0x01.toByte(), *ByteArray(16) { 0x00.toByte() })
+                    gatt?.writeCharacteristic(cmd)
+                    println("written characteristics")
+                }
+
+                override fun onCharacteristicChanged(gatt: BluetoothGatt?, characteristic: BluetoothGattCharacteristic?) {
+                    println("response: ${characteristic?.value?.toHexString() ?: "no response"}")
+                }
+
+            })
 
 
         }
--- app/src/main/java/fueltheburn/fueltheburn/extensions/GattConnectionExtensions.kt
+++ app/src/main/java/fueltheburn/fueltheburn/extensions/GattConnectionExtensions.kt
@@ -1,89 +1,74 @@
 package fueltheburn.fueltheburn.extensions
 
-import android.bluetooth.BluetoothGattCharacteristic
-import android.bluetooth.BluetoothGattService
-import android.util.Log
-import com.beepiz.blegattcoroutines.genericaccess.GenericAccess
-import com.beepiz.bluetooth.gattcoroutines.BGC
-import com.beepiz.bluetooth.gattcoroutines.GattConnection
-import fueltheburn.fueltheburn.CRC32
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.CancellationException
-import java.lang.Exception
-import java.lang.NullPointerException
-import java.lang.Thread.sleep
-import java.nio.ByteBuffer
 import java.util.*
 
 private val COPPERHEAD_COMMAND_UUID = UUID.fromString("c7d25540-31dd-11e2-81c1-0800200c9a66")
 private val COPPERHEAD_RESPONSE_UUID = UUID.fromString("d36f33f0-31dd-11e2-81c1-0800200c9a66")
 private val COPPERHEAD_SERVICE_UUID = UUID.fromString("83cdc410-31dd-11e2-81c1-0800200c9a66")
 
-suspend fun GattConnection.sendByteArray(serviceUUID: UUID, characteristicUUID: UUID, data: ByteArray): ByteArray {
-    println("Sending byte array ${data.toHexString()}")
-    val service = this.getService(serviceUUID)!!
-    val responseCharacteristic = service.getCharacteristic(COPPERHEAD_RESPONSE_UUID)
-    val responseChannel = this.openNotificationSubscription(responseCharacteristic)
-    val characteristic: BluetoothGattCharacteristic = service.getCharacteristic(characteristicUUID)
-    characteristic.value = data
-    this.writeCharacteristic(characteristic)
-    println("Sent byte array ${data.toHexString()}")
-    sleep(3000)
-    println("orange")
-    val response = responseChannel.receive().value
-    println("banana")
-    responseChannel.cancel()
-    return response
-}
-
-fun GattConnection.authenticate() {
-    try {
-        GlobalScope.launch {
-            try {
-                with(GenericAccess) {
-                    this@authenticate.connect()
-                    val t = this@authenticate.discoverServices()
-
-                    val connection = this@authenticate
-                    val copperheadService: BluetoothGattService = this@authenticate.getService(COPPERHEAD_SERVICE_UUID)!!
-                    val responseCharacteristic = copperheadService.getCharacteristic(COPPERHEAD_RESPONSE_UUID)
-                    this@authenticate.setCharacteristicNotificationsEnabledOnRemoteDevice(responseCharacteristic, true)
-
-
-                    val auth: ByteArray = byteArrayOf(0x90.toByte(), 0x01.toByte(), 0x01.toByte(), *ByteArray(16) { 0x00.toByte() })
-
-                    val nonceResponseBytes = this@authenticate.sendByteArray(COPPERHEAD_SERVICE_UUID, COPPERHEAD_COMMAND_UUID, auth)
-                    println(nonceResponseBytes.toHexString())
-                    val nonce = nonceResponseBytes.takeLast(16).toByteArray()
-
-                    val auth_token = ByteArray(16) { 0xFF.toByte() }
-
-                    val crc: CRC32 = CRC32()
-                    crc.update(nonce)
-                    crc.update(auth_token)
-
-                    val challengeResponse: Short = (0xFFFF and crc.getValue().toInt() xor (0xFFFF and crc.getValue().ushr(16).toInt())).toShort();
-                    val challengeResponseBuffer: ByteBuffer = ByteBuffer.allocate(19)
-                    challengeResponseBuffer.put(byteArrayOf(0xB0.toByte(), 0x03.toByte(), 0x02.toByte()))
-                    challengeResponseBuffer.putShort(challengeResponse)
-                    val authResponseBytes = this@authenticate.sendByteArray(COPPERHEAD_SERVICE_UUID, COPPERHEAD_COMMAND_UUID, challengeResponseBuffer.array())
-                    println("Printing final auth response: ")
-                    println(authResponseBytes.toHexString())
-                    println("Printed final auth response: ")
-                }
-            } catch (e: Exception) {
-                println("catch")
-                println(e.message)
-            } finally {
-                println("finally")
-                this@authenticate.close()
-            }
-        }
-    } catch (e: NullPointerException) {
-        Log.e("FuelTheBurn", "Some copperhead service doesn't exist, shits fucked")
-    }
-
-
-}
+//suspend fun GattConnection.sendByteArray(serviceUUID: UUID, characteristicUUID: UUID, data: ByteArray): ByteArray {
+//    println("Sending byte array ${data.toHexString()}")
+//    val service = this.getService(serviceUUID)!!
+//    val responseCharacteristic = service.getCharacteristic(COPPERHEAD_RESPONSE_UUID)
+//    val responseChannel = this.openNotificationSubscription(responseCharacteristic)
+//    val characteristic: BluetoothGattCharacteristic = service.getCharacteristic(characteristicUUID)
+//    characteristic.value = data
+//    this.writeCharacteristic(characteristic)
+//    println("Sent byte array ${data.toHexString()}")
+//    sleep(3000)
+//    println("orange")
+//    val response = responseChannel.receive().value
+//    println("banana")
+//    responseChannel.cancel()
+//    return response
+//}
+//
+//fun GattConnection.authenticate() {
+//    try {
+//        GlobalScope.launch {
+//            try {
+//                with(GenericAccess) {
+//                    this@authenticate.connect()
+//                    val t = this@authenticate.discoverServices()
+//
+//                    val connection = this@authenticate
+//                    val copperheadService: BluetoothGattService = this@authenticate.getService(COPPERHEAD_SERVICE_UUID)!!
+//                    val responseCharacteristic = copperheadService.getCharacteristic(COPPERHEAD_RESPONSE_UUID)
+//                    this@authenticate.setCharacteristicNotificationsEnabledOnRemoteDevice(responseCharacteristic, true)
+//
+//
+//                    val auth: ByteArray = byteArrayOf(0x90.toByte(), 0x01.toByte(), 0x01.toByte(), *ByteArray(16) { 0x00.toByte() })
+//
+//                    val nonceResponseBytes = this@authenticate.sendByteArray(COPPERHEAD_SERVICE_UUID, COPPERHEAD_COMMAND_UUID, auth)
+//                    println(nonceResponseBytes.toHexString())
+//                    val nonce = nonceResponseBytes.takeLast(16).toByteArray()
+//
+//                    val auth_token = ByteArray(16) { 0xFF.toByte() }
+//
+//                    val crc: CRC32 = CRC32()
+//                    crc.update(nonce)
+//                    crc.update(auth_token)
+//
+//                    val challengeResponse: Short = (0xFFFF and crc.getValue().toInt() xor (0xFFFF and crc.getValue().ushr(16).toInt())).toShort();
+//                    val challengeResponseBuffer: ByteBuffer = ByteBuffer.allocate(19)
+//                    challengeResponseBuffer.put(byteArrayOf(0xB0.toByte(), 0x03.toByte(), 0x02.toByte()))
+//                    challengeResponseBuffer.putShort(challengeResponse)
+//                    val authResponseBytes = this@authenticate.sendByteArray(COPPERHEAD_SERVICE_UUID, COPPERHEAD_COMMAND_UUID, challengeResponseBuffer.array())
+//                    println("Printing final auth response: ")
+//                    println(authResponseBytes.toHexString())
+//                    println("Printed final auth response: ")
+//                }
+//            } catch (e: Exception) {
+//                println("catch")
+//                println(e.message)
+//            } finally {
+//                println("finally")
+//                this@authenticate.close()
+//            }
+//        }
+//    } catch (e: NullPointerException) {
+//        Log.e("FuelTheBurn", "Some copperhead service doesn't exist, shits fucked")
+//    }
+//
+//
+//}
